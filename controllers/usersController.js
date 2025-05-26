fc17a59d4e3cefef1d07beb1e66c521cf8dd7d9d
practice <khushalkothiya5@gmail.com>
2025-05-26 13:34:26 +0530

    userupdate,deletePost,deleteUser

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -102,31 +102,112 @@ exports.forgotPassword = (req, res) => {
 
 // create post api
 exports.createPost = (req, res) => {
-  const { title, content } = req.body;
+  try {
+    const { title, content } = req.body;
 
-  user_id = req.user.id;
+    user_id = req.user.id;
 
-  if (!title || !content) {
-    return res.status(400).json({ message: "title and content are required" });
-  }
-
-  db.query(
-    "INSERT INTO posts (title,content,user_id) VALUES (?,?,?)",
-    [title, content, user_id],
-    (err, result) => {
-      if (err) return res.status(500).json({ error: err });
-      res.status(201).json({ message: "post created successfully" });
+    if (!title || !content) {
+      return res
+        .status(400)
+        .json({ message: "title and content are required" });
     }
-  );
+
+    db.query(
+      "INSERT INTO posts (title,content,user_id) VALUES (?,?,?)",
+      [title, content, user_id],
+      (err, result) => {
+        if (err) return res.status(500).json({ error: err });
+        res.status(201).json({ message: "post created successfully" });
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({ error: err.message });
+  }
 };
 
 // create getpost api
 exports.getPosts = (req, res) => {
-  db.query(
-    `SELECT posts.*,users.name AS author FROM posts JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC`,
-    (err, results) => {
-      if (err) return res.status(500).json({ error: err.message });
-      res.json(results);
-    }
-  );
+  try {
+    db.query(
+      `SELECT posts.*,users.name AS author FROM posts JOIN users ON posts.user_id = users.id ORDER BY posts.created_at DESC`,
+      (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(results);
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({ error: err.message });
+  }
 };
+
+//update post
+exports.updatePost = (req, res) => {
+  try {
+    const postId = req.params.id;
+    const { title, content } = req.body;
+    const userId = req.user.id;
+
+    if (!title || !content)
+      return res.status(400).json({ message: "Title and content required" });
+
+    const sql =
+      "UPDATE posts SET title = ?, content = ? WHERE id = ? AND user_id = ?";
+    db.query(sql, [title, content, postId, userId], (err, result) => {
+      if (err) return res.status(500).json({ error: err.message });
+
+      if (result.affectedRows === 0)
+        return res
+          .status(403)
+          .json({ message: "You can only edit your own posts" });
+
+      res.json({ message: "Post updated successfully" });
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+//delete posts
+exports.deletePost = (req, res) => {
+  userId = req.user.id;
+  deleteId = req.params.id;
+
+  const sql = "DELETE FROM posts WHERE user_id = ? AND id = ?";
+
+  db.query(sql, [userId, deleteId], (err, result) => {
+    if (err) {
+      return res.status(400).json({ message: "error in deleting post" });
+    }
+    if (result.affectedRows === 0) {
+      return res
+        .status(404)
+        .json({ message: "no matching found to be geleted" });
+    }
+    return res.status(200).json("post deleted successfully");
+  });
+};
+
+//delete users
+exports.deleteUser = (req, res) => {
+  const userId = req.user.id;
+  const deleteId = req.params.id; 
+
+  if (userId != deleteId) {
+    return res.status(400).json({ message: "Your ID does not match" });
+  }
+
+  db.query("DELETE FROM users WHERE id = ?", [deleteId], (err, result) => {
+    if (err) {
+      return res.status(400).json({ Error: err.message });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  });
+};
+
+//get user post by id
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -6,6 +6,9 @@ const {
   forgotPassword,
   createPost,
   getPosts,
+  updatePost,
+  deletePost,
+  deleteUser,
 } = require("../controllers/usersController");
 const { authenticateToken } = require("../middleware/authenticateToken");
 
@@ -14,5 +17,8 @@ router.post("/login", login);
 router.post("/forgotPassword", forgotPassword);
 router.post("/createPost", authenticateToken, createPost);
 router.get("/posts", authenticateToken, getPosts);
+router.put("/updatePost/:id", authenticateToken, updatePost);
+router.delete("/deletePost/:id", authenticateToken, deletePost);
+router.delete("/deleteUser/:id", authenticateToken, deleteUser);
 
 module.exports = router;
